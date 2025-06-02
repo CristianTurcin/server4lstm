422012601b9b862b166d9bcf4dc17b53feeee27f
Turcin Cristian <cristian.turcin@student.upt.ro>
2025-06-03 02:27:52 +0300

    modified model save

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 # Căi către model și scaler
-MODEL_PATH = 'bilstm_model.h5'
+MODEL_PATH = 'bilstm_model'
 SCALER_PATH = 'scaler.save'
 
 # Încarcă modelul și scalerul
